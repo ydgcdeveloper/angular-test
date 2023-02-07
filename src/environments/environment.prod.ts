2718754eeff14c098328647cee532321971840fc
Yan David <yan.gomez@qualud.es>
2023-02-07 17:49:58 -0500

    add baseUrl to env variables file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://lzrqaxbfjb.execute-api.us-east-2.amazonaws.com/develop/api/v2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBaseUrl: 'https://lzrqaxbfjb.execute-api.us-east-2.amazonaws.com/develop/api/v2'
 };
 
 /*
