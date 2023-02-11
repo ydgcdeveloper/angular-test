281d036ee92718cc419f55681584f6704fe8dbc1
Yan David <yan.gomez@qualud.es>
2023-02-10 21:27:21 -0500

    add page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
-  { path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
