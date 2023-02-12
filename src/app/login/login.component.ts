0bf9e152e97ce6e44414931a770aa61ca2ac820f
Yan David <yan.gomez@qualud.es>
2023-02-12 15:33:45 -0500

    set login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,7 @@
+import { CommonService } from './../service/common/common.service';
+import { AuthService } from './../service/auth/auth.service';
+import { LoginInput } from './../interface/login-input';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private commonService: CommonService
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    })
   }
 
+  ngOnInit(): void {
+
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+
+      const loginInput: LoginInput = {
+        username: this.email?.value,
+        password: this.password?.value
+      }
+
+      try {
+        this.authService.login(loginInput).then(((result) => {
+          if (!result.message) {
+            const token = result.data.token;
+            if (token) {
+              this.authService.saveToken(token);
+              this.commonService.showNotification('noti-success', 'Successfully logged in!');
+            }
+          }
+        }))
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,3 +1,5 @@
+import { AuthService } from './../service/auth/auth.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +13,10 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: []
 })
 export class LoginModule { }
